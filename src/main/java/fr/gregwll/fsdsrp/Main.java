64fda50ae40c8fd2e0081a28ad206d2e21e957f2
Grégoire Wild <115501088+NothingSpeci4l@users.noreply.github.com>
2025-07-03 20:36:10 +0200

    Dir system

--- src/main/java/fr/gregwll/fsdsrp/Main.java
+++ src/main/java/fr/gregwll/fsdsrp/Main.java
@@ -4,21 +4,40 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import fr.gregwll.fsdsrp.api.SimbriefAPI;
 import fr.gregwll.fsdsrp.api.SimbriefUtils;
+import fr.gregwll.fsdsrp.files.serialization.SettingsSerializationManager;
 import fr.gregwll.fsdsrp.utils.Logger;
 import fr.gregwll.fsdsrp.window.MainFrame;
+
+import java.io.File;
 
 public class Main {
 
     private static Logger logger = new Logger("FsDsRichpresence");
+
+    private static SettingsSerializationManager settingsSerializationManager;
 
     public static void main(String[] args) throws Exception {
         System.out.println(SimbriefUtils.getflightnum("chifou"));
 
         MainFrame.display();
 
+        new File(System.getProperty("user.home"), "/fsdsrp/").mkdirs();
+        File saveDir = new File(System.getProperty("user.dir"), "/fsdsrp/");
+        saveDir.mkdir();
+
+        File settingsFile = new File(saveDir, "settings.json");
+        if(!settingsFile.exists()) {
+            settingsFile.createNewFile();
+        }
+
+
     }
 
     public static Logger getLogger() {
         return logger;
+    }
+
+    public static SettingsSerializationManager getSettingsSerializationManager() {
+        return settingsSerializationManager;
     }
 }
